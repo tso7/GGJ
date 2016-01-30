f92302e108976b985e4c920618f4fc43fdc08daf
Abhishek Singh <abhishek.8172@gmail.com>
2016-01-30 02:18:14 -0500

    Initial scene setup

---
+++ fireboat/Assets/Resources/Scripts/BoatMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoatMovement : MonoBehaviour {
+
+    // Boat
+    public float moveSpeed;
+    
+    // Bob the boat up and down
+    public float boatBobSpeed;
+    public float boatBobDamper = 5;
+
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        float newPositionY = Mathf.Sin(Time.time * boatBobSpeed);
+        transform.position = startPosition + Vector3.up * (newPositionY / boatBobDamper);
+        Debug.Log(Vector3.up * (newPositionY / boatBobDamper));
+        Debug.Log("Position: "+transform.position);
+        transform.position += Vector3.right * moveSpeed * Time.time;
+
+     }
+}
---
+++ fireboat/Assets/Resources/Scripts/WaveScroller.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaveScroller : MonoBehaviour
+{
+    // Make the waves scroll across the screen
+    public float scrollSpeed;
+    public float tileSizeZ;
+
+    // Bob the waves up and down
+    public float bobSpeed;
+    public float bobDamper = 3;
+    
+
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        float newPositionZ = Mathf.Repeat(Time.time * scrollSpeed, tileSizeZ);
+        transform.position = startPosition + Vector3.right * newPositionZ;
+          
+        float newPositionY = Mathf.Sin(Time.time * bobSpeed);
+        transform.position += Vector3.up * (newPositionY/bobDamper);
+    }
+}
